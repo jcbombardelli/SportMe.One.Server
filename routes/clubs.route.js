b945055ffc1e04d62daa194cebf6e8fc3f0f1dca
JC Bombardelli <jc.bombardelli@live.com>
2017-06-19 00:40:05 -0300

    Filtro de clubes atualizado

--- routes/clubs.route.js
+++ routes/clubs.route.js
@@ -2,7 +2,7 @@ module.exports = function (app, _db) {
 
     app.get('/api/clubs', function (req, res) {
 
-        _db.collection('clube').find().toArray(function (err, result) {
+        _db.collection('clube').find().project({ Clube: 1, _id: 0 }).toArray(function (err, result) {
             if (err) {
                 throw err;
             }
