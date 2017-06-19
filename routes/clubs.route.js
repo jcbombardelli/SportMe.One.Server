3a653d4d114ff688c94febe1a130812dc14b4154
JC Bombardelli <jc.bombardelli@live.com>
2017-06-19 12:12:38 -0300

    Filtro de setores por clube

--- routes/clubs.route.js
+++ routes/clubs.route.js
@@ -10,4 +10,17 @@ module.exports = function (app, _db) {
         });
 
     });
+
+
+    app.get('/api/clubs/sectors', function (req, res) {
+
+        var club = req.param('club');
+
+        _db.collection('clube').find({ Clube: club }).project({ Setores: 1, _id:0 }).toArray(function (err, result) {
+            if (err) {
+                throw err;
+            }
+            res.send(result[0].Setores);
+        });
+    });
 }
