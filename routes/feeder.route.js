17152f6963edfd82eccf33d754a2568378260714
JC Bombardelli <jc.bombardelli@live.com>
2017-06-19 00:40:23 -0300

    validação em Feeder login

--- routes/feeder.route.js
+++ routes/feeder.route.js
@@ -8,6 +8,42 @@ module.exports = function (app, _db) {
             }
             res.send(result);
         });
+    });
 
+
+    app.get('/api/feeder/verify', function (req, res) {
+
+        var user = req.param('user');
+        var pass = req.param('pass');
+
+        _db.collection('feeder').findOne({ User: user, Pass: pass }, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            if (result != null) {
+                if (result.User = user && result.Pass == pass)
+                    res.send('true');
+                else
+                    res.send('false');
+            }
+            else {
+                res.send('false');
+            }
+
+
+        });
+
+
+    });
+
+    app.get('/api/feeder/login', function (req, res) {
+
+        _db.collection('feeder').find().project({ User: 1, Pass: 1, _id: 0 }).toArray(function (err, result) {
+            if (err) {
+                throw err;
+            }
+            res.send(result);
+        });
     });
 }
